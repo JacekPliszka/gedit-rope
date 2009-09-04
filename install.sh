b99ec26e04b6dd3b4075ffa22f8c1a74c21230a3
nbchtu <nbchtu@gmail.com>
2009-09-05 02:36:11 +0400

    cleanup

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
-
 GEDIT_PLUGINS_PATH="$HOME/.gnome2/gedit/plugins/"
-echo "Installing to $GEDIT_PLUGINS_PATH"
 cp ropeplugin.gedit-plugin $GEDIT_PLUGINS_PATH
 cp -r ropeplugin $GEDIT_PLUGINS_PATH
 
